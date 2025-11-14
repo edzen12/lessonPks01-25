92abf805155ec405096a889285dbd5d59b5358d2
edzen12 <oichiev.edzen@gmail.com>
2025-11-14 11:55:58 +0600

    сделал books, Вам надо сделать Restor.py

--- fe.py
+++ fe.py
@@ -1,9 +1,9 @@
 class Tour:
     def __init__(self, id, price, days):
-        self.__id = id 
-        self.__price = price 
-        self._is_booked = False 
-        self._client = None 
+        self.__id = id
+        self.__price = price
+        self._is_booked = False
+        self._client = None
         self._days = days 
     
     @property
@@ -13,55 +13,44 @@ class Tour:
     @property
     def price(self):
         return self.__price
-
+    
     @price.setter
     def price(self, value):
-        if value >= 5000:
+        if value>=5000:
             self.__price = value
         else:
             print("цена не может быть ниже 5000 сом")
 
-    def book(self, client): #бронирует тур, делает его недоступным, если клиент оплатил.
-        pass 
-    def cancel_booking(self): #отменяет бронь, делает тур доступным.
-        pass 
-    def info(self): #краткая информация о туре. 
+
+    def book(self, client): #бронирует тур, делает его недоступным
         pass 
 
-class Client:
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance 
+    def cancel_booking(self): #отменяет бронь, делает тур доступным
+        pass 
 
-    def pay(self, amount):
-        if self.balance>=amount:
-            self.balance-=amount
-            return self.balance
-        else:
-            return False
-        
-    def add_balance(self, amount):
-        if amount >0:
-            self.balance += amount
-        else:
-            return False
-        
-    def info(self):
-        return f"Имя: {self.name} Баланс: {self.balance}сом"
+    def info(self): 
+        pass 
 
-# 3. Класс Agency
+# class Client:
 # Атрибуты:
-# name — название агентства;
-# tours — список объектов Tour;
-# _income — защищённый атрибут (доход агентства).
+# name — имя клиента;
+# balance — баланс клиента.
 
+# Методы:
+# pay(amount) — уменьшает баланс, если хватает денег;
+# add_balance(amount) — пополнение счёта;
+# info() — возвращает строку с именем и балансом.
+
+
+class Agency:
+    def __init__(self, name):
+        self.name = name 
+        self.tours = [] 
+        self._income = 0  #доход агентства
+ 
 # Методы:
 # add_tour(tour) — добавляет новый тур;
 # show_available_tours() — показывает все свободные туры;
 # book_tour(client, tour_id) — бронирует тур для клиента;
 # cancel_all_bookings() — отменяет все активные брони;
 # show_status() — показывает состояние всех туров и текущую выручку.
-
-
-
-# https://github.com/edzen12/lessonPks01-25/
